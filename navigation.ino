f6034ee2d5425d7e7db9a0c117a697a7b3a55ec6
Stewart Lilley <stew_lilley@hotmail.com>
2012-08-07 16:41:59 -0500

    Initial Development Release
    This is the initial development release.  It has a whole bunch of the
    ArduPlane code removed and a tiny bit of new code added, mainly in the
    attitude control routine.  This build will compile without errors but is
    totally untested.  Use only as a basis for further development, not
    testing.

---
+++ navigation.ino
@@ -0,0 +1,16 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+static long wrap_360(long error)
+{
+	if (error > 36000)	error -= 36000;
+	if (error < 0)		error += 36000;
+	return error;
+}
+
+static long wrap_180(long error)
+{
+	if (error > 18000)	error -= 36000;
+	if (error < -18000)	error += 36000;
+	return error;
+}
+
---
+++ sensors.ino
@@ -0,0 +1,63 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+// Sensors are not available in HIL_MODE_ATTITUDE
+#if HIL_MODE != HIL_MODE_ATTITUDE
+
+void ReadSCP1000(void) {}
+
+static void init_barometer(void)
+{
+    barometer.calibrate(mavlink_delay);
+    ahrs.set_barometer(&barometer);
+    gcs_send_text_P(SEVERITY_LOW, PSTR("barometer calibration complete"));
+}
+
+// filter altitude from the barometer with a 0.3 low pass
+// filter
+static LowPassFilterInt32 altitude_filter(0.3);
+
+// read the barometer and return the updated altitude in centimeters
+// above the calibration altitude
+static int32_t read_barometer(void)
+{
+	barometer.read();
+    return altitude_filter.apply(((int32_t)barometer.get_altitude() * 100.0));
+}
+
+// in M/S * 100
+static void read_airspeed(void)
+{
+    airspeed.read();
+//	calc_airspeed_errors();
+}
+
+static void zero_airspeed(void)
+{
+    airspeed.calibrate(mavlink_delay);
+}
+
+#endif // HIL_MODE != HIL_MODE_ATTITUDE
+
+static void read_battery(void)
+{
+	if(g.battery_monitoring == 0) {
+		battery_voltage1 = 0;
+		return;
+	}
+	
+	if(g.battery_monitoring == 3 || g.battery_monitoring == 4) {
+        static AP_AnalogSource_Arduino bat_pin(BATTERY_PIN_1);
+		battery_voltage1 = BATTERY_VOLTAGE(bat_pin.read_average());
+    }
+	if(g.battery_monitoring == 4) {
+        static AP_AnalogSource_Arduino current_pin(CURRENT_PIN_1);
+		current_amps1	 = CURRENT_AMPS(current_pin.read_average());
+		current_total1	 += current_amps1 * (float)delta_ms_medium_loop * 0.0002778;				// .0002778 is 1/3600 (conversion to hours)
+	}
+	
+	#if BATTERY_EVENT == ENABLED
+		if(battery_voltage1 < LOW_VOLTAGE)	low_battery_event();
+		if(g.battery_monitoring == 4 && current_total1 > g.pack_capacity)	low_battery_event();
+	#endif
+}
+
